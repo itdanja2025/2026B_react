a59bd5f77040c8628fced8ecb66b532e3ce61ca4
itdanja2025 <itdanja2025@gmail.com>
2026-09-15 11:02:21 +0900

    day49 2h

--- src/main.jsx
+++ src/main.jsx
@@ -36,5 +36,11 @@ const create = createRoot( root );
 // import Component1 from "./example/day02/Component1";
 // create.render( <Component1/> )
 
-import Component2 from "./example/day02/Component2";
+// import Component2 from "./example/day02/Component2";
+// create.render( <Component2/> )
+
+// import Component1 from "./example/day03/Component1";
+// create.render( <Component1/> )
+
+import Component2 from "./example/day03/Component2";
 create.render( <Component2/> )
